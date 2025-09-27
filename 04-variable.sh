90a405ed9410ae0f60b467858d0763eabb8f3deb
TulasiLatha <lathagangadhari98@gmail.com>
2025-09-27 18:54:01 +0530

    variable

--- 04-variable.sh
+++ 04-variable.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
+
 $PERSON1=$1
 $PERSON2=$2
 
 echo "$PERSON1:: Welcome to Alaska $PERSON2"
-echo "$PERSON2:: Thank you $PERSON2, finally not on Zoom webinar"
+echo "$PERSON2:: Thank you $PERSON1, finally not on Zoom webinar"
 echo "$PERSON1:: so, do we have a deal"
 echo "$PERSON2:: Yes, if you make Ukraine joins Russia"
