5dd71daf921e53aefa8eceea533ac69870212c9c
TulasiLatha <lathagangadhari98@gmail.com>
2025-09-27 19:04:17 +0530

    variables

--- 05-variables.sh
+++ 05-variables.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 echo "Please enter your PIN Number"
-read PIN_NUMBER # here PIN_NUMBER is avariable
+read -s PIN_NUMBER # here PIN_NUMBER is avariable
 
 echo "PIN entered is : $PIN_NUMBER"
