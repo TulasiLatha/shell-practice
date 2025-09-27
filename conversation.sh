70e0fc40eb519c92c6e420bcc1d93140ef26bcfa
TulasiLatha <lathagangadhari98@gmail.com>
2025-09-27 18:14:32 +0530

    some message

--- conversation.sh
+++ conversation.sh
@@ -3,4 +3,4 @@
 echo "Ramesh:: Hi Suresh, How are you?"
 echo "Suresh:: Hello Ramesh, Iam fine.How are you?"
 echo "Ramesh:: what are you learning recently"
-echo: "Suresh :: Iam learning Shell scripting Ramesh"
+echo "Suresh:: Iam learning Shell scripting Ramesh"
