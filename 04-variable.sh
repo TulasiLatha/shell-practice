bd79173256fea828db6f79274bc8ba9bcafc6bca
TulasiLatha <lathagangadhari98@gmail.com>
2025-09-27 18:46:58 +0530

    variables

---
+++ 04-variable.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+$PERSON1=Trump
+$PERSON2=Putin
+
+echo "$PERSON1:: Welcome to Alaska $PERSON2"
+echo "$PERSON2:: Thank you $PERSON2, finally not on Zoom webinar"
+echo "$PERSON1:: so, do we have a deal"
+echo "$PERSON2:: Yes, if you make Ukraine joins Russia"
