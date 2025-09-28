457a4f26aad72073a4f49782b3d1682e609e4440
TulasiLatha <lathagangadhari98@gmail.com>
2025-09-28 21:10:30 +0530

    variables

--- 15-colors.sh
+++ 15-colors.sh
@@ -1,48 +1,49 @@
 #!/bin/bash
 
+USERID=$(id -u)
 R="\e[31m"
 G="\e[32m"
 Y="\e[33m"
 N="\e[0m"
 
-
-USERID=$(id -u)
-
 if [ $USERID -ne 0 ]; then
-     echo "ERROR:: Please run this script with root privelege"
-     exit 1 # failure is other than 0
+    echo "ERROR:: Please run this script with root privelege"
+    exit 1 # failure is other than 0
 fi
 
-VALIDATE(){
-   if [ $1 -ne 0 ]; then
-         echo -e "Installing $2...$R is failure $N"
-         exit 1
+VALIDATE(){ # functions receive inputs through args just like shell script args
+    if [ $1 -ne 0 ]; then
+        echo -e "Installing $2 ... $R FAILURE $N"
+        exit 1
     else
-         echo -e "Installing $2.... $G success $N"
+        echo -e "Installing $2 ... $G SUCCESS $N"
     fi
 }
 
 dnf list installed mysql
-if[ $? -ne 0]; then
+# Install if it is not found
+if [ $? -ne 0 ]; then
     dnf install mysql -y
-    VALIDATE $? "MYSQL"
+    VALIDATE $? "MySQL"
 else
-    echo "MYSQL already exists ... $Y SKIPPIMG $N"
+    echo -e "MySQL already exist ... $Y SKIPPING $N"
 fi
 
 dnf list installed nginx
-if[ $? -ne 0 ]; then
+if [ $? -ne 0 ]; then
     dnf install nginx -y
-    VALIDATE $? "NGINX"
-else 
-    echo "NGINX already exist ... $Y Skipping  $N"
+    VALIDATE $? "Nginx"
+else
+    echo -e "Nginx already exist ... $Y SKIPPING $N"
 fi
+
 dnf list installed python3
 if [ $? -ne 0 ]; then
     dnf install python3 -y
-    VALIDATE $? "MONGODB"
-else 
-    echo -e "NGINX already exists .... $Y Skipping $N"
+    VALIDATE $? "python3"
+else
+    echo -e "Python3 already exist ... $Y SKIPPING $N"
 fi
+
 
 
