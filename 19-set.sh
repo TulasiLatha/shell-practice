58d9c232814a92f9c7f903ce21b5bb3e525051d8
TulasiLatha <lathagangadhari98@gmail.com>
2025-10-05 21:36:47 +0530

    session-6

--- 19-set.sh
+++ 19-set.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 set -e
+error(){
+ echo " here is an error"
+}
+trap error ERR
 
 echo "hello"
 echo "before error.."
-ccca:hgg
+ccca:hgg # here shell understands ther is an error and signal is ERR
 echo "after erro"
